99adcd059f360a02452d2412523fff4ae7a46d02
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-08 11:26:53 +0900

    append Drawers

--- lib/screens/TopScreen.dart
+++ lib/screens/TopScreen.dart
@@ -10,6 +10,27 @@ class TopScreen extends StatelessWidget {
         title: Row(
             children: [const Text('Top100 page')]
         )
-    ), body: Center(child: Text('Top100 Page')));
+    ), body: Center(child: Text('Top100 Page')),
+        drawer: Drawer(
+          child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+          DrawerHeader(
+          child: Text('Information'),
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+          ),
+        ),
+        ListTile(
+          title: Text('My Page'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+        title: Text('Data Source'),
+    onTap: () {
+      Navigator.pop(context);
+    })])));
   }
 }
