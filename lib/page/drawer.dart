dd69a1815ed84f1b625c4ebf00f1dfa5cb700f09
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-08 22:20:23 +0900

    drawer split

---
+++ lib/page/drawer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class customDrawer extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar : AppBar(
+        title: Row(
+            children: [const Text('Top100 page')]
+        )
+    ), body: Center(child: Text('Top100 Page')),
+        drawer: Drawer(
+            child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    child: Text('Information'),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('My Page'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                      title: Text('Data Source'),
+                      onTap: () {
+                        Navigator.pop(context);
+                      })])));
+  }
+}
