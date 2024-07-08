f6480f18a4676dd9b05dfb76ca85ea643072b881
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-08 14:04:44 +0900

    ListTile added

--- lib/screens/LeftDrawer.dart
+++ lib/screens/LeftDrawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 
 
@@ -16,6 +18,7 @@ class _LeftDrawerState extends State<LeftDrawer>{
   @override
   void initState(){
     super.initState();
+    DateTime news_date = DateTime.now();
   }
 
   @override
@@ -30,9 +33,12 @@ class _LeftDrawerState extends State<LeftDrawer>{
                 padding: EdgeInsets.zero,
                 children: [
                   DrawerHeader(
-                    child: Text('Information'),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
+                    child: Container(
+                      child: Text('Information'),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                      )
                     ),
                   ),
                   ListTile(
@@ -44,7 +50,55 @@ class _LeftDrawerState extends State<LeftDrawer>{
                   ListTile(
                       title: Text('Data Source'),
                       onTap: () {
-                        Navigator.pop(context);
-                      })])));
+                        _showAlertDialog(context, 'Sources Information',
+                            [{'text' : 'This price and the source of the news is Yahoo Finance'}]);
+                      }),
+                  ListTile(
+                    title: Text('Contact'),
+                    onTap: () {
+                      _showAlertDialog(context, 'Developer Info',[
+                        {'text': 'Github ', 'url': 'https://github.com/rlaalsrl0922'},
+                        {'text': 'Email ', 'url': 'mailto:rlaalsrl0922@khu.ac.kr'},
+                        {'text': 'LinkedIn ', 'url': 'https://www.linkedin.com/in/minki-kim-13892'},
+                        {'text': 'Resume ', 'url': 'https://superficial-waiter-55e.notion.site/1feb56ba63c8415e83d3302768f454bf?pvs=4'},]);
+                    },
+                  ),])));
+  }
+  void _showAlertDialog(BuildContext context, String title, List<Map<String, String>> contentList) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: contentList.map((content) {
+                return GestureDetector(
+                  onTap: () async {
+                    final String url = content['url']!;
+                    launchUrlString(url);
+                  },
+                  child: Text(
+                    content['text'] ?? '',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                );
+              }).toList(),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+        },
+    );
   }
 }
