091241d1af9d54a0c93d4f2b2c5cfe147c464eea
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-08 22:18:37 +0900

    screen

--- lib/screens/BookmarkScreen.dart
+++ lib/screens/BookmarkScreen.dart
@@ -1,57 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:myapp/screens/NewsScreen.dart';
 
 class BookmarkScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar : AppBar(
-        title: Row(
-            children: [const Text('Bookmark page')]
-        )
-    ),body: SafeArea(
-    top: true,
-    child: Column(
-    mainAxisSize: MainAxisSize.max,
-    children: [
-    Expanded(
-    child: SingleChildScrollView(
-    child: Column(
-    mainAxisSize: MainAxisSize.max,
-    children: [
-    Padding(
-    padding: EdgeInsetsDirectional.fromSTEB(15, 5, 0, 0),
-    child: Row(
-    mainAxisSize: MainAxisSize.max,
-    children: [
-    Text('Stock Price ',
-    style: TextStyle(
-    fontSize: 20,
-    color: Colors.black)
-    ),
-    Padding(
-    padding: EdgeInsetsDirectional.fromSTEB(0, 6, 0, 0),
-    child: Text('Stock change(+) (Stock change(%))'),
-    ),
-    ],
-    ),
-    ),
-    Padding(
-    padding: EdgeInsetsDirectional.fromSTEB(20, 20, 0, 0),
-    child: Row(
-    mainAxisSize: MainAxisSize.max,
-    children: [
-    Container(
-    width: 350,
-    height: 200,
-    child: Text("BookMarkScreen"),
-    ),
-    ],
-    ),
-    ),
-    ],
-    ),
-    ))])));
+    return Scaffold(
+        appBar: AppBar(title: Row(children: [const Text('Bookmark page')])),
+        body: SafeArea(
+            top: true,
+            child: Column(mainAxisSize: MainAxisSize.max, children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20, 20, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Container(
+                      width: 350,
+                      height: 30,
+                      child: Text("BookMarkScreen"),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(5, 20, 0, 0),
+                  child: CompanyNewsScreen(),
+                ),
+              )
+            ])));
   }
 }
