7477826c0b5a4f8f8f04f0497a5ae795c7b20352
rlaalsrl0922 <rlaalsrl0922@khu.ac.kr>
2024-07-09 15:37:18 +0900

    logout button added

--- lib/screens/LeftDrawer.dart
+++ lib/screens/LeftDrawer.dart
@@ -56,6 +56,17 @@ class _LeftDrawerState extends State<LeftDrawer>{
                 ]);
               },
             ),
+            ListTile(
+              title: Text('Logout'),
+              onTap: () {
+                _showLogoutDialog(context, 'Are you gonna Logout', [
+                  {'text': 'Github ', 'url': 'https://github.com/rlaalsrl0922'},
+                  {'text': 'Email ', 'url': 'mailto:rlaalsrl0922@khu.ac.kr'},
+                  {'text': 'LinkedIn ', 'url': 'https://www.linkedin.com/in/minki-kim-13892'},
+                  {'text': 'Resume ', 'url': 'https://superficial-waiter-55e.notion.site/1feb56ba63c8415e83d3302768f454bf?pvs=4'}
+                ]);
+              },
+            ),
           ]
       ),
     );
@@ -93,6 +104,35 @@ class _LeftDrawerState extends State<LeftDrawer>{
                   Navigator.of(context).pop();
                 },
               ),
+            ],
+          );
+        }
+    );
+  }
+
+  void _showLogoutDialog(BuildContext context, String title, List<Map<String, String>> contentList) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.pushNamed(context,'/login');
+                },
+              ),
+              TextButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
             ],
           );
         }
